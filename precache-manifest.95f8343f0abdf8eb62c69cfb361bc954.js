33a7266eb51d2ec63d678b0c8411d962beede6a3
MedAli-Hafi <hfdali123@gmail.com>
2019-10-24 12:59:45 +0200

    Updates

---
+++ precache-manifest.95f8343f0abdf8eb62c69cfb361bc954.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8f5498d454b2d40b61285b47ed60294",
+    "url": "/Docplanner/index.html"
+  },
+  {
+    "revision": "9016f024a4e594939a15",
+    "url": "/Docplanner/static/css/main.f2eb6209.chunk.css"
+  },
+  {
+    "revision": "acfde13f864c357d9f1c",
+    "url": "/Docplanner/static/js/2.c3aa216d.chunk.js"
+  },
+  {
+    "revision": "9016f024a4e594939a15",
+    "url": "/Docplanner/static/js/main.873df482.chunk.js"
+  },
+  {
+    "revision": "4f3c5ea5a5d35d5df0eb",
+    "url": "/Docplanner/static/js/runtime-main.136485df.js"
+  }
+]);
